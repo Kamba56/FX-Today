39d367d975f58cff1d5218c206d8eb3acf0d26ee
toma <tomajohn316@yahoo.com>
2022-08-03 15:02:55 +0100

    Install redux

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+
+const Header = () => (
+  <header>
+    <button type="button">
+      <IoIosArrowBack />
+    </button>
+
+    <div>
+      CryptoToday
+    </div>
+  </header>
+);
+
+export default Header;
---
+++ src/redux/crypto/crypto.js
@@ -0,0 +1,9 @@
+const LOAD_COINS = 'LOAD_COINS';
+const initial = [];
+
+const coinsReducer = (state = initial, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
