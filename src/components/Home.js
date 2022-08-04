19c675734e79061158fa27c49bcc6ae80351cf92
toma <tomajohn316@yahoo.com>
2022-08-04 10:35:37 +0100

    Add load API thunk

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const HomePage = () => (
-  <div>
-    <div>This is home page</div>
-    <NavLink to="/details">Details</NavLink>
-  </div>
-);
-
+const HomePage = () => {
+  const x = 5;
+  console.log(x);
+  return (
+    <div>
+      <div>This is home page</div>
+      <NavLink to="/details">Details</NavLink>
+    </div>
+  );
+};
 export default HomePage;
--- src/redux/crypto/crypto.js
+++ src/redux/crypto/crypto.js
@@ -1,6 +1,6 @@
 const LOAD_COINS = 'LOAD_COINS';
 const initial = [];
-const baseURL = 'api.coincap.io/v2/assets';
+// const baseURL = 'api.coincap.io/v2/assets';
 
 const coinsReducer = (state = initial, action) => {
   switch (action.type) {
@@ -17,8 +17,8 @@ export const loadCoin = (arr) => ({
   payload: arr,
 });
 
-export const loadCoinThunk = () => (dispatch) => fetch(baseURL)
-    .then((res) => res.json())
-    .then((data) => dispatch(loadCoin(data.data)));
+// export const loadCoinThunk = () => () => fetch(baseURL)
+//   .then((res) => res.json())
+//   .then((dat) => console.log(dat.data));
 
 export default coinsReducer;
